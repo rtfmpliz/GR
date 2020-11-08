549315a5d6c9a88765923870183f6084c202dc33
wahyu <wahyu@istw.co.id>
2020-11-08 15:58:47 +0700

    Book Listing OK

---
+++ Web/Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IRepository<Author> repoAuthor;
+        private readonly IRepository<Book> repoBook;
+
+        public BookController(IRepository<Author> repoAuthor,
+            IRepository<Book> repoBook)
+        {
+            this.repoAuthor = repoAuthor;
+            this.repoBook = repoBook;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<BookListingViewModel> model = new List<BookListingViewModel>();
+            repoBook.GetAll().ToList().ForEach(b => {
+                BookListingViewModel book = new BookListingViewModel
+                {
+                    Id = b.Id,
+                    BookName = b.Name,
+                    Publisher = b.Publisher,
+                    ISBN = b.ISBN
+                };
+                Author author = repoAuthor.Get(b.AuthorId);
+                book.AuthorName = $"{author.FirstName} {author.LastName}";
+                model.Add(book);
+            });
+            return View("Index", model);
+        }
+    }
+}
---
+++ Web/Models/BookListingViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class BookListingViewModel
+    {
+        public long Id
+        {
+            get;
+            set;
+        }
+        public string BookName
+        {
+            get;
+            set;
+        }
+        public string AuthorName
+        {
+            get;
+            set;
+        }
+        public string ISBN
+        {
+            get;
+            set;
+        }
+        public string Publisher
+        {
+            get;
+            set;
+        }
+    }
+}
